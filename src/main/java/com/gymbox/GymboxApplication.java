b3fec30e6b54d67316284ce4cb83d07aa29df6cb
Matias Portela <matiasportela94@gmail.com>
2021-08-13 21:21:57 -0300

    initial commit

---
+++ src/main/java/com/gymbox/GymboxApplication.java
@@ -0,0 +1,13 @@
+package com.gymbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymbox/gymbox/GymboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymbox.gymbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
